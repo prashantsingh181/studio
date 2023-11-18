886ccfb08e212979a8daa9abf353056f44ba64ad
prashantsingh181 <prashant.singh181@gmail.com>
2023-11-18 18:53:58 +0530

    basic structure ready

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const video = document.querySelector("#camera");
+const clickPicture = document.querySelector(".click-picture");
+const recordVideo = document.querySelector(".record-video");
 
-navigator.mediaDevices.enumerateDevices().then((data) => console.log(data));
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  });
+let currentFacingMode = "user";
+
+function toggleCamera() {
+  currentFacingMode = currentFacingMode === "user" ? "environment" : "user";
+  startCamera();
 }
+
+function startCamera() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: {
+        facingMode: currentFacingMode,
+      },
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((error) => {
+      console.error("Error accessing the camera:", error);
+    });
+}
+
+clickPicture.addEventListener("click", function () {
+  document.querySelector(".hero").style.display = "none";
+  startCamera();
+});
+recordVideo.addEventListener("click", function () {
+  document.querySelector(".hero").style.display = "none";
+  startCamera();
+});
